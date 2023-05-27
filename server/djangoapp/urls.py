14f4d030c6e62f0e5472b6ed3a972e27457e49b1
ozioma <christ.stephaug@gmail.com>
2023-05-27 11:43:51 +0200

    about update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,14 +10,19 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='', view=views.about, name='about'),
 
     # path for contact us view
+    path(route='', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
